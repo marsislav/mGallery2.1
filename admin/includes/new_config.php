0192b722e362970f69ef2e855c96c10e4b7b7200
marsislav <mkostadinov87@gmail.com>
2022-02-02 22:15:01 +0200

    Initial commit

---
+++ admin/includes/new_config.php
@@ -0,0 +1,10 @@
+<?php
+// Database connection constants
+
+//Enter your database credentials bellow:
+
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'root');
+define ('DB_PASS', '');
+define ('DB_NAME', 'gallery_db');
+
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,9 @@
+<nav>
+	<ul>
+		<li><a href="index.php">Dashboard</a></li>
+		<li><a href="users.php">Users</a></li>
+		<li><a href="upload.php">Upload</a></li>
+		<li><a href="photos.php">Photos</a></li>
+		<li><a href="comments.php">Comments</a></li>
+	</ul>
+</nav>
